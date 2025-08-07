419272fd4e377da6334431f4c4c6bdbed0f489ca
Abhiram Pinnaka <148970953+Abhiram1502@users.noreply.github.com>
2025-08-07 22:21:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ menuProfile.addEventListener('click', function() {
     academicsSection.classList.toggle('showSlide');
     console.log('Toggle button clicked');
   });
+  const MentorBtn =  document.getElementById('mentorBtn');
+  const mentorSection = document.getElementById('mentor-options');
+
+  MentorBtn.addEventListener('click', function () {
+    mentorSection.classList.toggle('showSlide');
+    console.log('Toggle button clicked');
+  });
+  document.addEventListener('click', function(event) {
+    const academicsSection = document.getElementById('academicsSection');
+    const mentorSection = document.getElementById('mentor-options');
+    if (!academicsSection.contains(event.target) && !toggleBtn.contains(event.target)) {
+        academicsSection.classList.remove('showSlide');
+    }
+    if (!mentorSection.contains(event.target) && !MentorBtn.contains(event.target)) {
+        mentorSection.classList.remove('showSlide');
+    }
+  });
+function mentorPage() {
+  document.querySelector('.mentor-page').style.display = 'flex';
+  document.querySelector('.home-page').style.display = 'none';
+  document.querySelector('.timetable-page').style.display = 'none';
+  console.log('Mentor page toggled');
+}
+
+function timetablePage() {
+  document.querySelector('.mentor-page').style.display = 'none';
+  document.querySelector('.home-page').style.display = 'none';
+  document.querySelector('.timetable-page').style.display = 'flex';
+  console.log('Time table page toggled');
+}
